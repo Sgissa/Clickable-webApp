e973d98c9cbc9632cb93e519a69c37987e9a1cf9
Sgissa <143265121+Sgissa@users.noreply.github.com>
2023-10-28 18:10:03 -0400

    Add files via upload

---
+++ JSON-Clicks/app/app.js
@@ -0,0 +1,125 @@
+var ITEMS = [
+    {
+       "itemTitle":"Axe",
+       "itemThumbImg":"sthumb1_150x150.jpg",
+       "itemFullImg":"thumb1_300x300.jpg",
+       "itemBriefDes":" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiam architecto amet ",
+       "itemDescription":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsumvoluptatibus earum dolor ab delectus aperiam architecto ametaspernatur? Earum iure doloremque eligendi quo maiores nesciunt quamquibusdam quidem mollitiaautem Lorem ipsum dolor sit amet consecteturadipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiamarchitecto amet aspernatur? Earum iure doloremque eligendi quo maiores nesciunt quam quibusdam quidem mollitiaautem",
+    },
+    {
+       "itemTitle":"Cooler",
+       "itemThumbImg":"sthumb2_150x150.jpg",
+       "itemFullImg":"thumb2_300x300.jpg",
+       "itemBriefDes":" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiam architecto amet ",
+       "itemDescription":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsumvoluptatibus earum dolor ab delectus aperiam architecto ametaspernatur? Earum iure doloremque eligendi quo maiores nesciunt quamquibusdam quidem mollitiaautem Lorem ipsum dolor sit amet consecteturadipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiamarchitecto amet aspernatur? Earum iure doloremque eligendi quo maiores nesciunt quam quibusdam quidem mollitiaautem",
+      
+    },
+    {
+       "itemTitle":"Fire Starter",
+       "itemThumbImg":"sthumb3_150x150.jpg",
+       "itemFullImg":"thumb3_300x300.jpg",
+       "itemBriefDes":" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiam architecto amet ",
+       "itemDescription":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsumvoluptatibus earum dolor ab delectus aperiam architecto ametaspernatur? Earum iure doloremque eligendi quo maiores nesciunt quamquibusdam quidem mollitiaautem Lorem ipsum dolor sit amet consecteturadipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiamarchitecto amet aspernatur? Earum iure doloremque eligendi quo maiores nesciunt quam quibusdam quidem mollitiaautem",
+       
+    },
+    {
+       "itemTitle":"Med Kit",
+       "itemThumbImg":"sthumb4_150x150.jpg",
+       "itemFullImg":"thumb4_300x300.jpg",
+       "itemBriefDes":" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiam architecto amet ",
+       "itemDescription":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsumvoluptatibus earum dolor ab delectus aperiam architecto ametaspernatur? Earum iure doloremque eligendi quo maiores nesciunt quamquibusdam quidem mollitiaautem Lorem ipsum dolor sit amet consecteturadipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiamarchitecto amet aspernatur? Earum iure doloremque eligendi quo maiores nesciunt quam quibusdam quidem mollitiaautem",
+       
+    },
+    {
+       "itemTitle":"Roasting Sticks",
+       "itemThumbImg":"sthumb5_150x150.jpg",
+       "itemFullImg":"thumb5_300x300.jpg",
+       "itemBriefDes":" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiam architecto amet ",
+       "itemDescription":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsumvoluptatibus earum dolor ab delectus aperiam architecto ametaspernatur? Earum iure doloremque eligendi quo maiores nesciunt quamquibusdam quidem mollitiaautem Lorem ipsum dolor sit amet consecteturadipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiamarchitecto amet aspernatur? Earum iure doloremque eligendi quo maiores nesciunt quam quibusdam quidem mollitiaautem",
+       
+    },
+    {
+       "itemTitle":"Sleeping Bag",
+       "itemThumbImg":"sthumb6_150x150.jpg",
+       "itemFullImg":"thumb6_300x300.jpg",
+       "itemBriefDes":" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiam architecto amet ",
+       "itemDescription":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsumvoluptatibus earum dolor ab delectus aperiam architecto ametaspernatur? Earum iure doloremque eligendi quo maiores nesciunt quamquibusdam quidem mollitiaautem Lorem ipsum dolor sit amet consecteturadipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiamarchitecto amet aspernatur? Earum iure doloremque eligendi quo maiores nesciunt quam quibusdam quidem mollitiaautem",
+    },
+    {
+       "itemTitle":"Stove",
+       "itemThumbImg":"sthumb7_150x150.jpg",
+       "itemFullImg":"thumb7_300x300.jpg",
+       "itemBriefDes":" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiam architecto amet ",
+       "itemDescription":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsumvoluptatibus earum dolor ab delectus aperiam architecto ametaspernatur? Earum iure doloremque eligendi quo maiores nesciunt quamquibusdam quidem mollitiaautem Lorem ipsum dolor sit amet consecteturadipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiamarchitecto amet aspernatur? Earum iure doloremque eligendi quo maiores nesciunt quam quibusdam quidem mollitiaautem",
+    },
+    {
+       "itemTitle":"Tent",
+       "itemThumbImg":"sthumb8_150x150.jpg",
+       "itemFullImg":"thumb8_300x300.jpg",
+       "itemBriefDes":" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiam architecto amet ",
+       "itemDescription":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsumvoluptatibus earum dolor ab delectus aperiam architecto ametaspernatur? Earum iure doloremque eligendi quo maiores nesciunt quamquibusdam quidem mollitiaautem Lorem ipsum dolor sit amet consecteturadipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiamarchitecto amet aspernatur? Earum iure doloremque eligendi quo maiores nesciunt quam quibusdam quidem mollitiaautem",
+    },
+    {
+       "itemTitle":"Trash Bin",
+       "itemThumbImg":"sthumb9_150x150.jpg",
+       "itemFullImg":"thumb9_300x300.jpg",
+       "itemBriefDes":" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiam architecto amet ",
+       "itemDescription":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsumvoluptatibus earum dolor ab delectus aperiam architecto ametaspernatur? Earum iure doloremque eligendi quo maiores nesciunt quamquibusdam quidem mollitiaautem Lorem ipsum dolor sit amet consecteturadipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiamarchitecto amet aspernatur? Earum iure doloremque eligendi quo maiores nesciunt quam quibusdam quidem mollitiaautem",
+    },
+    {
+       "itemTitle":"Water Bottle",
+       "itemThumbImg":"sthumb10_150x150.jpg",
+       "itemFullImg":"thumb10_300x300.jpg",
+       "itemBriefDes":" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiam architecto amet ",
+       "itemDescription":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsumvoluptatibus earum dolor ab delectus aperiam architecto ametaspernatur? Earum iure doloremque eligendi quo maiores nesciunt quamquibusdam quidem mollitiaautem Lorem ipsum dolor sit amet consecteturadipisicing elit. Ipsum voluptatibus earum dolor ab delectus aperiamarchitecto amet aspernatur? Earum iure doloremque eligendi quo maiores nesciunt quam quibusdam quidem mollitiaautem",
+       
+    }
+ ];
+
+
+function loadData(){
+    $.each(ITEMS, function(index, item){
+        $("#app").append(`<div class="content-container" >
+        <div id="${index}"  class="item-holder">
+        <h4 class="game-name">${item.itemTitle}</h4>
+          <img src="images/${item.itemThumbImg}" alt="${item.itemThumbImg}" />
+          <div class="quick-brief">
+          <div class="brief-des">
+            ${item.itemBriefDes}
+          </div>
+        </div>
+        </div>
+        
+      </div>`);
+    });
+
+    initListeners();
+}
+
+function initListeners(){
+    $(".item-holder").click(function(e){
+        let itemIndex = e.currentTarget.id;
+        console.log(itemIndex);
+        $("#app").html(`<div class="item-container">
+        <h2>${ITEMS [itemIndex].itemTitle}</h2>
+        <h4 class="item-name">Axe</h4>
+        <img src="images/${ITEMS[itemIndex].itemFullImg}" alt="axe-image" />
+        <div class="itemDes">
+          ${ITEMS[itemIndex].itemDescription}
+        </div>
+        <div class="close">Close</div></div>`);
+
+        addCloselistener();
+    });
+}
+
+function addCloselistener(){
+   $(".close").click(function(){
+      $("#app").html("");
+      loadData();
+   });
+}
+
+$(document).ready(function(){
+    loadData();
+});
